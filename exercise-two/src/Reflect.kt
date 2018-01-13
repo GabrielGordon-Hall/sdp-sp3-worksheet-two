900aa37523248fa6d9a3a52d5109d99aae8020b0
GabrielGordon-Hall <ggordo04@dcs.bbk.ac.uk>
2018-01-13 12:51:46 +0000

    'Two questions'

--- exercise-two/src/Reflect.kt
+++ exercise-two/src/Reflect.kt
@@ -1,11 +1,11 @@
-import kotlin.reflect.KClass
-import kotlin.reflect.full.createInstance
+import kotlin.reflect.full.primaryConstructor
 
 fun main(args: Array<String>) {
     if (args.size < 2) {
         println("Requires a class name and a list of arguments")
         System.exit(-1)
     }
-
-    // TODO
+    val targetClass = Class.forName(args[0]).kotlin
+    val classConstructor = targetClass.primaryConstructor
+    val classInstance = classConstructor.toString()
 }
