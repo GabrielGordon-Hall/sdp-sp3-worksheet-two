2c2ca5b9c20c1a9d451cce09aad405ca1daab4d4
GabrielGordon-Hall <ggordo04@dcs.bbk.ac.uk>
2018-01-15 16:27:20 +0000

    'Q4'

--- exercise-four/src/MyClass.kt
+++ exercise-four/src/MyClass.kt
@@ -1,4 +1,10 @@
-class MyClass(val first: String, val second: String) {
+class MyClass {
+    companion object {
+        @JvmField
+        var first = "Hello"
+        @JvmField
+        var second = "World"
+    }
 
     override fun toString(): String {
         return "MyClass(first='$first', second='$second')"
--- exercise-four/src/Reflect.kt
+++ exercise-four/src/Reflect.kt
@@ -2,12 +2,12 @@ import java.lang.reflect.Modifier
 
 fun <T: Any> toString(clazz: T) {
     val classObject = clazz::class
-    val classFields = classObject.java.declaredFields.filter { Modifier.isPrivate(it.modifiers) }
+    val classFields = classObject.java.declaredFields.filter { !Modifier.isPrivate(it.modifiers) }
     val combine = classFields.map { it.name }.zip(classFields.map { it.get(classObject) })
-    print(classObject.qualifiedName + "(" + combine.forEach { print(it.first + "=" + it.second + " , ") } + ")")
+    print(classObject.qualifiedName + "(" + combine.map { it.first + "=" + it.second } + ")")
 }
 
 fun main(args : Array<String>) {
-    val myClass = MyClass("Hello", "World")
+    val myClass = MyClass()
     toString(myClass)
 }
