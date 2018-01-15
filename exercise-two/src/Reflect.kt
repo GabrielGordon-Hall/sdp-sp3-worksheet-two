8ea77c3395aa8c40e16a00ccaadbec9b07c26e9c
GabrielGordon-Hall <ggordo04@dcs.bbk.ac.uk>
2018-01-15 13:01:05 +0000

    'Q4'

--- exercise-two/src/Reflect.kt
+++ exercise-two/src/Reflect.kt
@@ -1,15 +1,13 @@
-import kotlin.reflect.full.createInstance
-
 fun main(args: Array<String>) {
     if (args.size < 2) {
         println("Requires a class name and a list of arguments")
         System.exit(-1)
     }
-    val numberOfParams = args.size
     val targetClass = Class.forName(args[0]).kotlin
-    val classConstructors = targetClass.constructors
-    val constructor = classConstructors.first {
-        it.parameters.size == numberOfParams && it.parameters.all { it.type.equals(String) }
+    val arguments = args.sliceArray(1 until args.size)
+    val constructor = targetClass.constructors.first {
+        it.parameters.size == arguments.size && it.parameters.all { it.type.toString() == "kotlin.String" }
     }
-    val newInstance = targetClass.createInstance()
+    val newInstance = constructor.call(*arguments)
+    println(newInstance.toString())
 }
